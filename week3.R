832b8e7f2a6e94ed811c807b1ecf8253d3a60fc7
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-05-09 21:10:25 +1000

    Add files via upload

---
+++ week3.R
@@ -0,0 +1,24 @@
+library(foreign)
+library(xlsReadWrite) 
+library(ggplot2)
+
+setwd ("C:/Econometrics Applications")
+
+return <- read.table("return.csv",sep=",", header=TRUE)
+
+returnmodel <- lm(return~dkr + eps + netinc + salary, data= return)
+summary(returnmodel)
+
+returnmodel <- lm(return~dkr + eps + lnetinc + lsalary, data= return)
+summary(returnmodel)
+
+mba <- read.dta("mba3.dta")
+
+mbamodel <- lm(log(salary)~ testsc + wam + libsize + rank, data=mba)
+summary(mbamodel)
+anova(mbamodel)
+
+
+mbamodel2 <- lm(log(salary)~ log(libsize) + rank, data=mba)
+summary(mbamodel2)
+
