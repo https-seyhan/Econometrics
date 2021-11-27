3e93bfcdfad307fbb19b8079716b06843c0dea33
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-27 13:20:42 +1100

    Update AUC.R

--- AUC.R
+++ AUC.R
@@ -5,7 +5,6 @@ mytable <-read.table("stock.csv", sep=",", header=T)
 setwd("C:/Comp/")
 
 mytable <-read.table("kaggle_training_dataset_formatted2.txt", sep=",", header=T)
-
 prediction1<-c(rep(2,10),rep(1,10))
 
 lass1<-c(rep(1,5),rep(-1,5),rep(1,5),rep(-1,5))
