a19772285e6cc42f4d64b11f80cb32da686afcb0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-18 12:51:44 +1100

    Update VariousRegressionModels.R

--- VariousRegressionModels.R
+++ VariousRegressionModels.R
@@ -44,6 +44,7 @@ cbind(CO = coef(olsreg5),  confint(olsreg5))
 #Question6
 tls6 <- tsls(lwage ~ educ + exper + expersq + nc + south + west + nc18 + south18 + west18 + urban + urban18, ~ CUFees + exper + expersq + nc + south + west + nc18 + south18 + west18 + urban + urban18, data= nlsy13)
 summary(tls6)
+
 cbind(CO = coef(tls6),  confint(tls6))
 ivreg6 <-  ivreg(lwage ~ educ + exper + expersq + nc + south + west + nc18 + south18 + west18 + urban + urban18 | CUFees + exper + expersq + nc + south + west + nc18 + south18 + west18 + urban + urban18, data= nlsy13)
 summary(ivreg6)
