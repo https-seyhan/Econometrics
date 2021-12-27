0e5c81800f704ef4f952ef15d60bda67cfd837c6
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-27 11:29:06 +1100

    Update QuantileRegression.R

--- QuantileRegression.R
+++ QuantileRegression.R
@@ -22,7 +22,6 @@ olsreg2 <- lm(approve~ white + obrat+ loanprc + unem + male + married + dep + sc
 summary(olsreg2)
 coef(olsreg2)
 bptest(olsreg2)
-
 #GLM
 glm2a <- glm(approve ~ white, family=binomial(link=probit), data=loandata)
 summary(glm2a)
