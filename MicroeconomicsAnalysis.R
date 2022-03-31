deaaa86f702611dcfa45b13fce9ed5e219437ab4
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-31 11:15:23 +1100

    Update MicroeconomicsAnalysis.R

--- MicroeconomicsAnalysis.R
+++ MicroeconomicsAnalysis.R
@@ -31,6 +31,7 @@ summary(pwt)
 # select variables of interest
 variables <- c("country", "countrycode", "year", "rgdpe", "rgdpo", "emp", "pop", "avh",  
                "ck")
+
 pwt <- pwt[variables]
 
 # create variables for class with understandable labels 
