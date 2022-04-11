b6cad5a87e1a0800255e56fabc72d645d5d214ab
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-11 18:52:05 +1000

    Update FittingLinearModels.R

--- FittingLinearModels.R
+++ FittingLinearModels.R
@@ -1,5 +1,6 @@
 # Fitting Linear Models on house prices
 library(foreign)
 setwd ("C:/Econometrics Applications")
+
 houseprice <- read.dta("hprice2.dta")
 linerreg <- lm(price ~ sqrmtr + lotsize + bdrms, data=houseprice)
